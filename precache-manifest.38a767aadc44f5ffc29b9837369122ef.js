ac713497abbb26b3deeefc46adbad3936832af80
Prachi Meshram <prachimesh89@gmail.com>
2023-08-15 22:02:17 +0530

    Updates

---
+++ precache-manifest.38a767aadc44f5ffc29b9837369122ef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0941ab5297572a32a25",
+    "url": "/github.io/Portfolio/static/css/main.b9c70270.chunk.css"
+  },
+  {
+    "revision": "b0941ab5297572a32a25",
+    "url": "/github.io/Portfolio/static/js/main.b0941ab5.chunk.js"
+  },
+  {
+    "revision": "11419f9ab822d4f34c57",
+    "url": "/github.io/Portfolio/static/js/1.11419f9a.chunk.js"
+  },
+  {
+    "revision": "5bb47b3361bce15ed9fc",
+    "url": "/github.io/Portfolio/static/js/2.5bb47b33.chunk.js"
+  },
+  {
+    "revision": "02f380d816ee11823224",
+    "url": "/github.io/Portfolio/static/js/runtime~main.02f380d8.js"
+  },
+  {
+    "revision": "bf75a5b10d06277207d703cddd150d67",
+    "url": "/github.io/Portfolio/static/media/Prachiclear.bf75a5b1.jpeg"
+  },
+  {
+    "revision": "a79db6f9ef2a92903577f09a67dd4add",
+    "url": "/github.io/Portfolio/static/media/cIcon.a79db6f9.svg"
+  },
+  {
+    "revision": "5609795586e6ff02d0b0c8610c80f74b",
+    "url": "/github.io/Portfolio/static/media/cppIcon.56097955.svg"
+  },
+  {
+    "revision": "9a15aef14da9d64f28eb2364d498c575",
+    "url": "/github.io/Portfolio/static/media/cssIcon.9a15aef1.svg"
+  },
+  {
+    "revision": "fb4e5efd8d22fd24e7a22e9a1a35ff1b",
+    "url": "/github.io/Portfolio/static/media/githubIcon.fb4e5efd.svg"
+  },
+  {
+    "revision": "cc844a486abd9bb0dccf1462538f5be1",
+    "url": "/github.io/Portfolio/static/media/htmlIcon.cc844a48.svg"
+  },
+  {
+    "revision": "2978bf2f3807f7c8edcb4d21a7dd2760",
+    "url": "/github.io/Portfolio/static/media/jsIcon.2978bf2f.svg"
+  },
+  {
+    "revision": "0066978f4e7a0bb75e0cd64d8815a6a5",
+    "url": "/github.io/Portfolio/static/media/reactIcon.0066978f.svg"
+  },
+  {
+    "revision": "3b264caeaadd0418ee767cd4df854179",
+    "url": "/github.io/Portfolio/static/media/typescriptIcon.3b264cae.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/github.io/Portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "52d5c232737c4e547e6a115f2a8798ae",
+    "url": "/github.io/Portfolio/static/media/homeDecorationCompo.52d5c232.svg"
+  },
+  {
+    "revision": "ed3363877960b4fcf3681dc8ff19a2be",
+    "url": "/github.io/Portfolio/index.html"
+  }
+];
